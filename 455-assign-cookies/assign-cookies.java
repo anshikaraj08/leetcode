2c1f8bdf7bfcd4897d5e08559df292a62a38cc66
Anshika Raj <152436670+anshikaraj08@users.noreply.github.com>
2026-07-24 23:53:10 +0530

    Time: 14 ms (92.10%) | Memory: 51 MB (95.90%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,22 +1,40 @@
+// class Solution {
+//     public int findContentChildren(int[] g, int[] s) {
+//         Arrays.sort(g);
+//         Arrays.sort(s);
+//         int gp=0, sp=0;
+//         int count=0;
+
+//         while(gp < g.length && sp < s.length){
+//             if(s[sp]>=g[gp]){
+//                 count++;
+//                 sp++;
+//                 gp++;
+
+//             }else if(s[sp]<g[gp]){
+//                 sp++;
+//             }
+//         }
+//         return count;
+
+
+//     }
+// }
+
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        int gp=0, sp=0;
-        int count=0;
 
-        while(gp < g.length && sp < s.length){
-            if(s[sp]>=g[gp]){
-                count++;
-                sp++;
+        int gp = 0, sp = 0;
+
+        while (gp < g.length && sp < s.length) {
+            if (s[sp] >= g[gp]) {
                 gp++;
-
-            }else if(s[sp]<g[gp]){
-                sp++;
             }
+            sp++;
         }
-        return count;
 
-
+        return gp;
     }
 }
