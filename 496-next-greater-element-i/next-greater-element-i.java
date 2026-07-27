d987600e28dfd3407583ad77bc1de793ac22ce5f
Anshika Raj <152436670+anshikaraj08@users.noreply.github.com>
2026-07-27 16:39:58 +0530

    Time: 4 ms (73.86%) | Memory: 45.5 MB (22.57%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> st = new Stack<>();
+        HashMap<Integer,Integer> mp= new HashMap<>();
+        int n=nums2.length;
+
+
+        for(int i=n-1;i>=0;i--){
+            if(st.empty()){
+                mp.put(nums2[i],-1);
+                st.push(nums2[i]);
+            }
+            else if(!st.empty() && st.peek()<nums2[i]){
+                while(!st.empty() && st.peek()<nums2[i]){
+                    st.pop();
+                }
+                if(st.empty()){
+                    st.push(nums2[i]);
+                    mp.put(nums2[i],-1);
+                }else{//
+                    mp.put(nums2[i],st.peek());
+                    st.push(nums2[i]);
+
+                }
+            }
+            else if(!st.empty() && st.peek()>nums2[i]){
+                mp.put(nums2[i],st.peek());
+                st.push(nums2[i]);
+            }
+        } 
+        
+
+        // for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
+        //     System.out.println(entry.getKey() + " : " + entry.getValue());
+        // }
+        int[] ans = new int[nums1.length];
+        for(int i=0;i<nums1.length;++i){
+           ans[i]= mp.get(nums1[i]);
+        }
+        return ans;
+
+
+    }
+}
