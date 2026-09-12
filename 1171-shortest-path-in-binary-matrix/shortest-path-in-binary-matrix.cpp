dc7947953822c39eabc4028b097ac76663f3bbe7
Anshika Raj <152436670+anshikaraj08@users.noreply.github.com>
2026-09-12 23:13:22 +0530

    Time: 284 ms (5.04%) | Memory: 83 MB (5.00%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    typedef  pair<int,pair<int,int>> p;
     bool isSafe(int r, int c, int n, vector<vector<int>>& grid) {
         return r >= 0 && r < n &&
                c >= 0 && c < n &&
@@ -11,6 +12,8 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
+        if(m==0 || n==0 || grid[0][0]!=0) return -1;
+
         if(grid[0][0] == 1 || grid[n-1][n-1] == 1)
             return -1;
 
@@ -20,34 +23,40 @@ public:
             {1,-1},  {1,0},   {1,1}
         };
 
-        queue<pair<int,int>> q;
+        vector<vector<int>> res(m,vector<int>(n,INT_MAX));
 
-        q.push({0,0});
-        grid[0][0] = 1;
+        priority_queue<p,vector<p>,greater<p>> q;
 
-        int level = 0;
+        res[0][0]=0;
+        q.push({0,{0,0}}); 
+        grid[0][0]=1;
+
         while(!q.empty()){
-            int size=q.size();
-            while(size--){
-                auto f=q.front();
-                int r=f.first;
-                int c=f.second;
-                q.pop();
-                if(r==m-1 && c==n-1) return level+1;
+            auto f=q.top();
+            q.pop();
 
-                for(auto dir: directions){
-                    int nr=r+dir[0];
-                    int nc=c+dir[1];
 
-                    if(isSafe(nr,nc,n,grid)){
-                        q.push({nr,nc});
-                        grid[nr][nc] = 1;
+            int d=f.first;
 
-                    }
+            auto [r,c]=f.second;
+            for(auto dir: directions){
+                int nr=r+dir[0];
+                int nc=c+dir[1];
+                
+                if(isSafe(nr,nc,n,grid) && res[nr][nc] > d+1){
+                    res[nr][nc]=d+1;
+                    q.push({d+1,{nr,nc}});
+                    grid[nr][nc]=1;
                 }
+
+            
+
+
             }
-            level+=1;
+
         }
-        return -1;
+        return res[m-1][n-1] == INT_MAX ? -1 : res[m-1][n-1] + 1;
+
+        
     }
 };
